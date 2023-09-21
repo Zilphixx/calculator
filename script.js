a728b162f2cf27eadfa575bd2f5b4532c5e2eadb
Zilphixx <144513145+Zilphixx@users.noreply.github.com>
2023-09-21 14:37:50 +0800

    added operate function
    with some incomplete code

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ function multiplication(input1, input2) {
 function division(input1, input2) {
   return input1 / input2;
 }
+
+function operate(operator, input1, input2) {
+  let result;
+  if (addition) {
+
+  } else if (subtraction) {
+
+  } else if (multiplication) {
+
+  } else if (division) {
+
+  }
+  return result;
+}
