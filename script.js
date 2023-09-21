cf7f2a769fbec9cbd210e3bc36a67249d7554cb5
Zilphixx <144513145+Zilphixx@users.noreply.github.com>
2023-09-21 13:44:28 +0800

    added operate function

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const clearInput = document.querySelector('#clear');
 const deleteInput = document.querySelector('#delete');
 
 let valuesToOperate = [];
+let operation;
+let result;
 
 clearInput.addEventListener('click', () => {
   equalValue.textContent = '';
@@ -22,6 +24,8 @@ numpads.forEach((numpad) => {
     if (numpad.id === 'equals') {
       valuesToOperate.push(equalValue.textContent+inputValue.textContent);
       equalValue.textContent = `${valuesToOperate}${numpad.textContent}`;
+      result = operate(valuesToOperate);
+      inputValue.textContent = result;
     } else {
       inputValue.textContent += numpad.id;
     }
@@ -32,7 +36,34 @@ operators.forEach((operator) => {
   operator.addEventListener('click', () => {
     equalValue.textContent += `${inputValue.textContent}${operator.textContent}`;
     inputValue.textContent = '';
+    if (operator.id === 'plus') {
+      operation = '+';
+    } else if (operator.id === 'minus') {
+      operation = '-';
+    } else if (operator.id === 'multiply') {
+      operation = 'x';
+    } else if (operator.id === 'divide') {
+      operation = '÷';
+    }
   });
 });
+
+function operate(inputs) {
+  let inputHolder = inputs.toString();
+  let newValuesToOperate;
+  let result;
+
+  if (operation === '+') {
+    newValuesToOperate = inputHolder.split('+');
+  } else if (operation === '-') {
+    newValuesToOperate = inputHolder.split('-');
+  } else if (operation === 'x') {
+    newValuesToOperate = inputHolder.split('x');
+  } else if (operation === '÷') {
+    newValuesToOperate = inputHolder.split('÷');
+  }
+
+  return result;
+}
 
 
