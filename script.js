566e3ebb0128c1dd612590383ee58734307d5e90
Zilphixx <144513145+Zilphixx@users.noreply.github.com>
2023-09-21 15:10:30 +0800

    added the operations

--- script.js
+++ script.js
@@ -11,19 +11,68 @@ function multiplication(input1, input2) {
 }
 
 function division(input1, input2) {
-  return input1 / input2;
+  return (input1 / input2).toFixed(2);
 }
 
 function operate(operator, input1, input2) {
   let result;
-  if (addition) {
-
-  } else if (subtraction) {
-
-  } else if (multiplication) {
-
-  } else if (division) {
-
+  if (operator === 'addition') {
+    result = addition(input1, input2);
+  } else if (operator === 'subtract') {
+    result = subtraction(input1, input2);
+  } else if (operator === 'multiply') {
+    result = multiplication(input1, input2);
+  } else if (operator === 'division') {
+    result = division(input1, input2)
   }
   return result;
 }
+
+const clearButton = document.querySelector('#clear');
+const deleteButton = document.querySelector('#delete');
+const screenValue = document.querySelector('.number-container');
+const numpads = document.querySelectorAll('.numpads');
+const equalOperator = document.querySelector('#equals');
+const operators = document.querySelectorAll('.operations');
+
+let firstNumber;
+let secondNumber;
+let operation;
+
+numpads.forEach((numpad) => {
+  numpad.addEventListener('click', () => {
+    if(numpad.id === 'equals') {
+    } else {
+      screenValue.textContent += numpad.id;
+    }
+  });
+});
+
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    if (operator.id === 'plus') {
+      operation = 'addition';
+      firstNumber = screenValue.textContent;
+      screenValue.textContent = '';
+    } else if (operator.id === 'minus') {
+      operation = 'subtract';
+      firstNumber = screenValue.textContent;
+      screenValue.textContent = '';
+    } else if (operator.id === 'multi') {
+      operation = 'multiply';
+      firstNumber = screenValue.textContent;
+      screenValue.textContent = '';
+    } else if (operator.id === 'divide') {
+      operation = 'division';
+      firstNumber = screenValue.textContent;
+      screenValue.textContent = '';
+    }
+  });
+}); 
+
+equalOperator.addEventListener('click', () => {
+  secondNumber = screenValue.textContent;
+  +firstNumber;
+  +secondNumber;
+  screenValue.textContent = operate(operation, firstNumber, secondNumber);
+});
